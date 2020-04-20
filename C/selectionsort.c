3039fd430410d02fe9b4394815ed94119452ffa2
Duhan <48991386+duhan2@users.noreply.github.com>
2020-04-20 18:32:34 +0200

    Update selectionsort.c

--- C/selectionsort.c
+++ C/selectionsort.c
@@ -2,6 +2,48 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define N 100    //Makro für Arrraygröße
+
+//Funktion für Schritt 3
+void swap(int *x, int *y){      
+
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+
+}
+
+//Funktion für Schritt 2
+int findmin(int *array, int start, int ende){
+
+    int min_stelle;
+
+    min_stelle = start; 
+
+   for(int i = start; i <= ende; i++){
+
+        if(array[min_stelle] > array[i]){
+
+            min_stelle = i;
+
+        }
+
+    }
+
+    return min_stelle;
+}
+
+//Optionale Funktion für Ausgabe
+void print(int *array){
+
+    for(int i = 0; i < N; i++){
+
+        printf("Array[%d] = %d \n",i,array[i]);
+
+    }
+}
+
 
 int main(){
 
@@ -9,58 +51,40 @@ int main(){
     srand(time(0));
     
     //Array deklarieren
-    int array[10];
+    int array[N];
 
     //Array initialisierem
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < N; i++){
 
         array[i] = rand()%100; //Damit die Zahlen nicht grösser als 100
 
-    }
-
-    printf("Unsortierter Durchlauf\n");
-
+    }   
+    
     //Ausgabe des Arrays
-    for(int i = 0; i < 10; i++){
-
-        printf("Array[%d] = %d \n",i,array[i]);
-
-    }
+    printf("Unsortierter Durchlauf\n");
+    print(array);
     
     int min;        //Variablen ausserhalb der Schleifen deklariert
-    int temp;       // hat 2 Gründe
 
-    //Schritt 1
-    for(int i = 0; i < 10; i++){
+    clock_t t1 = clock();
 
-        min = i;
-        
-        //Schritt 2
-        for(int j = i + 1; j < 10; j++){
+    for(int i = 0; i < N-1; i++){
 
-            if(array[min] > array[j]){
+        min  = findmin(array,i,N-1); 
 
-                //Schritt 3
-                temp = array[min];
-                array[min] = array[j];
-                array[j] = temp;
-
-            }
-
-        }
+        swap(&array[min],&array[i]);
 
     }
+
+    clock_t t2 = clock();
+    double duration = (double)(t2 - t1)/(double)CLOCKS_PER_SEC;
+
+    printf("\nDie Laufzeit fuer %d Elemente betraegt: %4e\n",N,duration);
 
     printf("\nSortierter Durchlauf\n");
-
-    //Wieder ausgeben um zu checken ob das sortieren geklappt hat
-    for(int i = 0; i < 10; i++){
-
-        printf("Array[%d] = %d \n",i,array[i]);
-
-    }
-
-    printf("\nHallo Mo\n");
+     
+    //Wieder ausgeben um zu checken, ob das sortieren geklappt hat
+    print(array);
 
     return 0;
 }
