bd4c15fe529fd3fac7f3e95477772f4f9f300e0e
Duhan <48991386+duhan2@users.noreply.github.com>
2020-09-12 16:27:47 +0200

    Update h_stack_t.h

--- CPP/Stack_aufgeteilt/h_stack_t.h
+++ CPP/Stack_aufgeteilt/h_stack_t.h
@@ -1,8 +1,10 @@
 #include <iostream>
 
-using namespace std;
-
 #pragma once
+
+//using namespace std;
+
+template<class T>
 
 class stack_t {
 
@@ -23,9 +25,9 @@ public:
 
     bool isEmpty();
 
-    void push(float value);
+    void push(T value);
 
-    float top();
+    T top();
 
     void pop();
 
@@ -34,8 +36,110 @@ public:
 
 private:
 
-    float value = 0;
+    T value = 0;
     stack_t* next = NULL;
 
 };
+
+template<class T>
+
+bool stack_t<T>::isEmpty() {
+    if (head == NULL) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+template<class T>
+
+void stack_t<T>::push(T value) {
+
+    stack_t* element = new stack_t;
+    element->value = value;
+
+    if (isEmpty() == true) {
+        head = element;
+        return;
+    }
+
+    stack_t* temp = head;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = element;
+}
+
+template<class T>
+
+void stack_t<T>::print() {
+    stack_t* temp = head;
+    while (temp != NULL) {
+
+        std::cout << temp->value << std::endl;
+        temp = temp->next;
+
+    }
+}
+
+template<class T>
+ T stack_t<T>::top() {
+
+    try {
+
+        stack_t* temp = head;
+
+        if (temp == NULL) {
+            throw "Stack is empty";
+        }
+
+        while (temp->next != NULL) {
+
+            temp = temp->next;
+        }
+
+        return temp->value;
+
+    }
+    catch(const char* err_string){
+        std::cerr << err_string << std::endl;
+        return 0;
+    };
+    
+}
+
+template<class T>
+
+void stack_t<T>::pop() {
+
+    try{
+
+        stack_t* temp = head;
+
+        if (temp == NULL) {
+            throw "Stack is empty";
+            //return;
+        }
+
+        while (temp->next->next != NULL) {
+        
+            temp = temp->next;
+
+        }
+
+        stack_t* temp2 = temp->next;
+
+        temp->next = temp2->next;
+
+        delete temp2;
+
+    }
+    catch (const char* err_string) {
+        std::cerr << err_string << std::endl;
+    };
+}
+
 
